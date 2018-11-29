7f79923572813d05606d1cc5526c7f35932ae514
Kyle Tanemura <kyletanemura@gmail.com>
2018-11-28 17:42:56 -0800

    Testing cursor position saving

--- src/Editor.js
+++ src/Editor.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import OnlineList from './OnlineList';
+import $ from 'jquery';
 
 class Editor extends Component {
     constructor() {
         super();
         this.state = {
             text: '',
+            last_position: -1
         };
     }
 
@@ -16,15 +18,67 @@ class Editor extends Component {
         });
     }
 
+    
+    cursor_changed = (element) => {
+        var new_position = this.getCursorPosition(element);
+        if (new_position !== this.state.last_position) {
+            this.state.last_position = new_position;
+            return true;
+        }
+            return false;
+    }
+    
+    setCursorPos = (input, start, end) => {
+        if (arguments.length < 3) end = start;
+        if ("selectionStart" in input) {
+            setTimeout(function() {
+                input.selectionStart = start;
+                input.selectionEnd = end;
+            }, 1);
+        }
+        else if (input.createTextRange) {
+            var rng = input.createTextRange();
+            rng.moveStart("character", start);
+            rng.collapse();
+            rng.moveEnd("character", end - start);
+            rng.select();
+        }
+    }
+
+    getCursorPosition= (element) => {
+        var el = $(element).get(0);
+        var pos = 0;
+        if ('selectionStart' in el) {
+            pos = el.selectionStart;
+        } else if ('selection' in document) {
+            el.focus();
+            var Sel = document.selection.createRange();
+            var SelLength = document.selection.createRange().text.length;
+            Sel.moveStart('character', -el.value.length);
+            pos = Sel.text.length - SelLength;
+        }
+        return pos;
+    }
+
     componentDidMount() {
         this.props.socket.on("UpdateText", data => {
             this.setState({
                 text: data
             });
+            this.setCursorPos($('#editor'), this.state.last_position);
         });
 
         this.setState({
             text: this.props.text
+        });
+        let that = this;
+        $('#editor').bind("keydown click focus", function() {
+            if (that.cursor_changed($('#editor'))) {
+                that.setState({
+                    last_position: that.getCursorPosition($('#editor'))
+                });
+            }
+            // console.log(that.cursor_changed($('#editor')));
         });
     }
 
@@ -37,9 +91,7 @@ class Editor extends Component {
                         users={this.props.users}
                     />
                 </div>
-                <div>
-                    <textarea class="chat" value={this.state.text} onChange={this.handleChange}></textarea>
-                </div>
+                    <textarea id="editor" class="chat" value={this.state.text} onChange={this.handleChange}></textarea>
             </div>
         );
     }
